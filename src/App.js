b75eed0a63fdc5954282b66e1d7438689244ed11
Brandon Fajardo <brandonfajardo@Brians-Macbook.local>
2018-12-19 10:44:05 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ class App extends Component {
     return (
       <Fragment>
         <Header />
+        <div className="row">
+          <div className="col-2-of-4">
+            <p>i'm in a row</p>
+          </div>
+          <div className="col-2-of-4">
+            <p>i'm in a row</p>
+          </div>
+        </div>
       </Fragment>
     );
   }
